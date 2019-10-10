059d937e4badd7f90e9576c80e3f488f87652749
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2019-10-09 20:34:39 -0500

    Update DetailActivity.java

--- PopularMoviesApp/app/src/main/java/com/example/popularmoviesapp/DetailActivity.java
+++ PopularMoviesApp/app/src/main/java/com/example/popularmoviesapp/DetailActivity.java
@@ -13,10 +13,13 @@ public class DetailActivity extends AppCompatActivity {
     public static final String MOVIE_RATING = "movie_rating";
     public static final String DESCRIPTION = "description";
     public static final String MOVIE_POSTER = "movie_poster";
-
+    public static final int MOVIE_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        //TODO Make a separate service request to the following URL in Detail Activity. https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&append_to_response=videos,reviews
+        //Allowed values=trailer*********************???????????????
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
@@ -27,8 +30,11 @@ public class DetailActivity extends AppCompatActivity {
         String movieRating = getIntent().getExtras().getString(MOVIE_RATING);
         String description = getIntent().getExtras().getString(DESCRIPTION);
         String moviePoster = getIntent().getExtras().getString(MOVIE_POSTER);
+        int movieId = getIntent().getExtras().getInt(String.valueOf(MOVIE_ID));
+        //If i just pass in the ID from the Main activity, then detail activity can turn into a service call from the new URL, taking in the ID
+        //and setting it equal to a variable that will get appended to the new URL. So now I have the URL, but how to extract the JSON data??
 
-        Movie chosenMovie = new Movie(movieTitle, releaseDate, movieRating, description, moviePoster);
+        Movie chosenMovie = new Movie(movieTitle, releaseDate, movieRating, description, moviePoster, movieId);
 
         if (chosenMovie == null) {
             closeOnError();
